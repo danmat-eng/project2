5aedbfaafd1d76229c8cccc473aabc9eb5decc69
danmat-eng <danmath29@nuevaschool.org>
2026-04-29 10:56:23 -0700

    Commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react'
 import axios from 'axios' // Make sure you've run: npm install axios
 
 function App() {
+  const [newLog, setNewLog] = useState(0)
   const [greeting, setGreeting] = useState("Loading...")
 
   useEffect(() => {
@@ -15,9 +16,11 @@ function App() {
   }, [])
 
   return (
-    <div style={{ textAlign: 'center', marginTop: '50px' }}>
-      <h1>MERN Stack Test</h1>
-      <p>Message from Backend: <strong>{greeting}</strong></p>
+    <div>
+        <h1 className='titleText'>Flight Log Tracker</h1>
+        <button className='buttonNewText' onClick={() => setNewLog(newLog + 1)}>
+          Added New Tasks: {newLog}
+        </button>
     </div>
   )
 }
